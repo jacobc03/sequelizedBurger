00fe8a92d94ae60ad06e7694cd2587ab98124749
Jacob <jacob@Jacobs-MacBook-Air.local>
2017-02-09 15:12:44 -0500

    burgers display

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,23 +6,10 @@ var burger = require('./../models/burger.js');
 module.exports = function(app) {
 	// This takes the user to the homepage
 	app.get('/', function(req, res) {
-		//homePage() is called here
-		burger.homePage(req, res);
+		burger.findAll({}).then(function(results) {
+		res.render("index", {burgers: results});
+	 })
 	})
 
-	// This post happens when the devour button is pushed
-	app.post('/delete/:id', function(req, res) {
-		// this var is created to store the id of the burger the user wants to devour
-		var id = req.params.id;
-		// the id var is past into the function below and on to updateOne() so the DataBase can be updated
-		burger.devour(req, res, id);
-	})
 
-	// This post happens when the order button is pushed
-	app.post('/', function(req, res) {
-		// This var is created to store string the user enters into the form
-		var newestBurger= req.body.newestBurger;
-		// the newestBurger var is past into the function below and on to insertOne() so the DataBase can be updated
-		burger.newBurger(req, res, newestBurger);
-	})
 }
